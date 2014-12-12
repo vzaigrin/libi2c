0204ac58c69070a306827d33ec9a69c89026c8e6
Vadim Zaigrin <vzaigrin@yandex.ru>
2014-12-12 13:47:41 +0300

    Adopted to 8-bit slave address in FreeBSD 10.1

--- libi2c.c
+++ libi2c.c
@@ -7,12 +7,12 @@ char i2c_read ( int fd, uint16_t slave, uint8_t offset, uint8_t *buf )  {
   struct iic_msg msg[2];
   struct iic_rdwr_data rdwr;
 
-  msg[0].slave = slave;
+  msg[0].slave = slave << 1 | !IIC_M_RD;
   msg[0].flags = !IIC_M_RD;
   msg[0].len = sizeof( offset );
   msg[0].buf = &offset;
 
-  msg[1].slave = slave;
+  msg[1].slave = slave << 1 | IIC_M_RD;
   msg[1].flags = IIC_M_RD;
   msg[1].len = sizeof( buf );
   msg[1].buf = buf;
@@ -37,8 +37,8 @@ char i2c_write ( int fd, uint16_t slave, uint8_t offset, uint8_t val )  {
 
   buf[0] = offset;
   buf[1] = val;
-  msg.slave = slave;
-  msg.flags = 0;
+  msg.slave = slave << 1 | !IIC_M_RD;
+  msg.flags = !IIC_M_RD;
   msg.len = sizeof( buf );
   msg.buf = buf;
 
@@ -52,3 +52,4 @@ char i2c_write ( int fd, uint16_t slave, uint8_t offset, uint8_t val )  {
   
   return(0);
 }
+
